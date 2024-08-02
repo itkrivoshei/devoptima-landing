26159e9af31c7643d9951e03f462cc7087d6be26
itkrivoshei <nikitakrivoshei@gmail.com>
2024-08-02 15:51:49 +0200

    chore: Update tailwind.config.js with new colors, fonts, and max widths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-bg': '#0A0811',
+        'text-primary': '#EFF1F6',
+        'text-secondary': '#BDC4D1',
+        'hover-bg': '#243BB9',
+        'hover-border': '#3F57DA',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        title: ['Aeonik', 'sans-serif'],
+      },
+      maxWidth: {
+        container: '1472px',
+        card: '380px',
+        section: '1220px',
+      },
+    },
   },
   plugins: [],
 };
