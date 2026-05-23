d86a707fd45b7df216ac02883266fffe77f076f4
Nikita Krivoshei <69799139+itkrivoshei@users.noreply.github.com>
2026-05-23 15:03:09 +0200

    Add CRA public URL compatibility for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/devoptima-landing/',
+  define: {
+    'process.env.PUBLIC_URL': JSON.stringify('/devoptima-landing'),
+  },
 });
